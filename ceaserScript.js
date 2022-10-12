021bdea924f13918a32c09da4f542d92f22bd1b7
UmidBaz <91578399+UmidBaz@users.noreply.github.com>
2022-10-12 01:40:20 -0400

    Add files via upload

--- ceaserScript.js
+++ ceaserScript.js
@@ -1,26 +1,56 @@
 var el = document.getElementById('ceasar');
-if(el){
-  el.addEventListener('click', runCeaser);
-}
+el.addEventListener('click', runCeaser);
+
 function runCeaser(){
     console.log("im in!")
     shift = document.getElementById("shift").value;
-
-    var message = document.getElementsByName("alice2").value;
-    var alphabet = "abcdefghijklmnopqrstuvwxyz";
-    var newalpha = "";
-    
-    //Creat new alpha
-    for (let i = 0; i < alphabet.length; i++){
-		let offset = (i + shift) % alphabet.length;
-		newalpha += alphabet[offset];
-	}
-
-    let result = "";
-    message = message.toLowerCase();
-    for (let i = 0; i < message.length; i++){
-        let index = alphabet.indexOf(message[i]);
-        result += newalpha[index];
+    if(shift > 100){
+      alert('Try keeping it less than 100!');
+      return false;
     }
-    console.log(result)
+    console.log(shift)
+    var message = document.getElementById("alice2").value;
+    
+    var enc = encrypt(message, shift);
+    var dec = decrypt(enc, shift);
+    document.getElementById("attacker2").innerHTML = enc;
+    document.getElementById("bob2").innerHTML = dec;
+}
+
+function encrypt(message,shift){
+  message.toLowerCase();
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var cipher = "";
+  for(var i = 0; i < message.length; i++){
+    var charPos = alphabet.indexOf(message.charAt(i));
+    var newShift = (Number(shift) + Number(charPos)) % 26;
+    if(message.charAt(i) == ' '){
+      cipher += ' ';
+    }
+    else{
+      var replaced = alphabet.charAt(newShift)
+      cipher += replaced;
+    }
+  }
+  return cipher;
+}
+function decrypt(message,shift){
+  message.toLowerCase();
+  var deciphered = "";
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  for(var i = 0; i < message.length; i++){
+    var charPos = alphabet.indexOf(message.charAt(i));
+    var newShift = (Number(charPos) - Number(shift)) % 26;
+    if (newShift < 0) {
+      newShift = alphabet.length + newShift;
+    }
+    if(message.charAt(i) == ' '){
+      deciphered += ' ';
+    }
+    else{
+      var replaced = alphabet.charAt(newShift);
+      deciphered += replaced;
+    }
+  }
+  return deciphered;
 }
